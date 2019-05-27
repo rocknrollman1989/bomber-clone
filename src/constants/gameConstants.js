dadc1be91e8f8ef33e94afedfdb446bd9937f970
RocknrollMan <rocknrollman1989@gmail.com>
2019-05-27 10:33:38 +0300

    add createObstacle

---
+++ src/constants/gameConstants.js
@@ -0,0 +1,4 @@
+export const GAME_BORDER = 15;
+export const GAME_SQUARE_WIDTH = 30;
+export const GAME_SQUARE_HEIGHT = 30;
+export const GAME_SQUARE = GAME_SQUARE_WIDTH * GAME_SQUARE_HEIGHT; // height * width;
--- src/script/components/GameWindow.js
+++ src/script/components/GameWindow.js
@@ -1,5 +1,8 @@
 import './game_window.css';
-import { generateCoordinates, getAmountOfObstacles } from '../../helpers/index';
+import { getAmountOfObstacles } from '../../helpers/index';
+import {
+  GAME_BORDER, GAME_SQUARE, GAME_SQUARE_WIDTH, GAME_SQUARE_HEIGHT,
+} from '../../constants/gameConstants';
 
 class CreateObstacles {
   constructor(displayInfo) {
@@ -16,39 +19,63 @@ class CreateObstacles {
     const obstacle = document.createElement('div');
     CreateObstacles.idCounter += 1;
     obstacle.className = 'obstacle';
-    obstacle.style.width = '30px';
-    obstacle.style.height = '30px';
-    const coordinat = generateCoordinates(realGameFieldWidth, realGameFieldHeight, CreateObstacles.obstaclesCoordinates);
-    CreateObstacles.obstaclesCoordinates.push({ top: coordinat.top, left: coordinat.left });
-    obstacle.style.top = `${coordinat.top}px`;
-    obstacle.style.left = `${coordinat.left}px`;
+    obstacle.style.width = `${GAME_SQUARE_WIDTH}px`;
+    obstacle.style.height = `${GAME_SQUARE_HEIGHT}px`;
+    obstacle.style.top = `${CreateObstacles.lastTopPoint}px`;
+    obstacle.style.left = `${CreateObstacles.lastLeftPoint}px`;
+    CreateObstacles.obstaclesArray.push({ top: CreateObstacles.lastTopPoint, left: CreateObstacles.lastLeftPoint, id: CreateObstacles.idCounter });
+    CreateObstacles.lastLeftPoint += 2 * GAME_SQUARE_WIDTH;
+    if (CreateObstacles.lastLeftPoint >= realGameFieldWidth) {
+      if (CreateObstacles.lastTopPoint > realGameFieldHeight - GAME_SQUARE_HEIGHT) return;
+      CreateObstacles.lineCounter += 1;
+      CreateObstacles.lastTopPoint += GAME_SQUARE_HEIGHT;
+      CreateObstacles.lastLeftPoint = CreateObstacles.lineCounter % 2 ? CreateObstacles.lastLeftPoint = GAME_BORDER : CreateObstacles.lastLeftPoint = GAME_BORDER + GAME_SQUARE_WIDTH;
+    }
     obstacle.innerHTML = this.obstacleId;
     document.body.appendChild(obstacle);
   }
 }
 
 CreateObstacles.idCounter = 0;
-CreateObstacles.obstaclesCoordinates = [];
+CreateObstacles.obstaclesArray = [];
+CreateObstacles.lineCounter = 1;
+CreateObstacles.lastTopPoint = GAME_BORDER;
+CreateObstacles.lastLeftPoint = GAME_BORDER + GAME_SQUARE_WIDTH * 2;
 
 class GameWindow {
   constructor() {
     this.displayHeight = window.innerHeight;
     this.displayWidth = window.innerWidth;
-    const borderForWindow = 15;
-    const gameWindow = document.createElement('div');
-    gameWindow.id = 'game_window';
-    gameWindow.style.width = `${this.displayWidth}px`;
-    gameWindow.style.height = `${this.displayHeight}px`;
-    gameWindow.style.border = `${borderForWindow}px solid #fad2d2`;
-    this.realGameFieldWidth = this.displayWidth - borderForWindow * 2;
-    this.realGameFieldHeight = this.displayHeight - borderForWindow * 2;
-    document.body.appendChild(gameWindow);
+    this.borderForWindow = GAME_BORDER;
+    this.gameSquare = GAME_SQUARE;
+    this.gameWindow = document.createElement('div');
+    this.createGameWindow();
     this.createObstaclesForGame();
   }
 
+  createGameWindow() {
+    this.gameWindow.id = 'game_window';
+    let neededWidth = this.displayWidth;
+    let neededheigth = this.displayHeight;
+    while (neededWidth % GAME_SQUARE_WIDTH) {
+      neededWidth -= 1;
+    }
+    while (neededheigth % GAME_SQUARE_HEIGHT) {
+      neededheigth -= 1;
+    }
+    this.gameWindow.style.width = `${neededWidth}px`;
+    this.gameWindow.style.height = `${neededheigth}px`;
+    this.gameWindow.style.border = `${this.borderForWindow}px solid #fad2d2`;
+    this.realGameFieldWidth = neededWidth - this.borderForWindow * 2;
+    this.realGameFieldHeight = neededheigth - this.borderForWindow * 2;
+    document.body.appendChild(this.gameWindow);
+  }
+
   createObstaclesForGame() {
-    const { realGameFieldWidth, realGameFieldHeight } = this;
-    const amountOfObstacles = getAmountOfObstacles(realGameFieldWidth, realGameFieldHeight);
+    const {
+      realGameFieldWidth, realGameFieldHeight, gameSquare,
+    } = this;
+    const amountOfObstacles = getAmountOfObstacles(realGameFieldWidth, realGameFieldHeight, gameSquare);
     for (let i = 0; i < amountOfObstacles; i += 1) {
       new CreateObstacles({ realGameFieldWidth, realGameFieldHeight });
     }
